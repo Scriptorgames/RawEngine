45fe4fd0fb72d8b2c4d0184086fb918edb6b88f6
Felix Schreiber <83061405+Scriptor25@users.noreply.github.com>
2024-06-30 10:29:32 +0200

    hopefully changed git submodule branches

--- RawEngine/include/RawEngine/MeshLoader.hpp
+++ RawEngine/include/RawEngine/MeshLoader.hpp
@@ -1,11 +1,24 @@
 #pragma once
 
+#include <cstdint>
 #include <string>
+#include <vector>
+#include <glm/glm.hpp>
 
 namespace RawEngine
 {
+    struct Vertex
+    {
+        alignas(16) glm::vec3 Position;
+        alignas(16) glm::vec3 Normal;
+        alignas(8) glm::vec2 UV;
+    };
+
     struct MeshLoader
     {
         void Load(const std::string& filename);
+
+        std::vector<Vertex> Vertices;
+        std::vector<uint32_t> Indices;
     };
 }
--- RawEngine/src/mesh_loader.cpp
+++ RawEngine/src/mesh_loader.cpp
@@ -2,4 +2,8 @@
 
 void RawEngine::MeshLoader::Load(const std::string& filename)
 {
+    Vertices.clear();
+    Indices.clear();
+
+    
 }
